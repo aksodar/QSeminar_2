fc0b0d177051f44af1a4e1754f411bfabcf54f8e
Anton Vinogradov <AVlVinogradov@sberbank.ru>
2023-01-23 18:47:07 +0300

    tasks from day 2 finished

--- src/main/java/ru/sberbank/Task1.java
+++ src/main/java/ru/sberbank/Task1.java
@@ -1,9 +1,14 @@
 package ru.sberbank;
 
+import ru.sberbank.data.TaskService;
+
 public class Task1 {
 
     public static void main(String[] args) {
-
+        TaskService t1 = new TaskService();
+        t1.createTask(7, "new");
+        System.out.println(t1.getTask("old"));
+        System.out.println(t1.getTask("new").toString());
+        System.out.println(t1.getTasksForDeveloping().toString());
     }
-
 }
--- src/main/java/ru/sberbank/data/Task.java
+++ src/main/java/ru/sberbank/data/Task.java
@@ -5,6 +5,15 @@ public class Task {
     boolean isDeveloped;
     String summary;
 
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", isDeveloped=" + isDeveloped +
+                ", summary='" + summary + '\'' +
+                '}';
+    }
+
     public Task(int id, String summary) {
         this.id = id;
         this.summary = summary;
