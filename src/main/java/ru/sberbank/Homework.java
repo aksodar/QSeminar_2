85afba3b008844f1890ab1a943457a8b99f34f5b
Damir Iskakov <dziskakov@sberbank.ru>
2023-02-05 12:52:51 +0300

    create and demo TesterService

--- src/main/java/ru/sberbank/Homework.java
+++ src/main/java/ru/sberbank/Homework.java
@@ -1,8 +1,28 @@
 package ru.sberbank;
 
+import ru.sberbank.data.Developer;
+import ru.sberbank.data.Task;
+import ru.sberbank.data.Tester;
+import ru.sberbank.data.TesterService;
+
 public class Homework {
 
     public static void main(String[] args) {
+        Developer developer = new Developer(1);
+        developer.addTask(new Task(1, "Check"));
+        developer.makeTask();
+        Task toTest = developer.getCurrentTask();
+        TesterService testerService = new TesterService();
+        System.out.println("Free testers:" +  testerService.getFreeTesters());
+        testerService.createTester(1, "Damir", "Iskakov");
+        testerService.createTester(2, "John", "Dow");
+        System.out.println("Free testers:" +  testerService.getFreeTesters());
+        Tester testerJohn = testerService.getTester("John", "Dow");
+        testerJohn.addTask(toTest);
+        System.out.println("Tester with task: " + testerJohn);
+        System.out.println("Find tester Johnny :" + testerService.getTester("Johnny", ""));
+        System.out.println("Free testers:" +  testerService.getFreeTesters());
+
     }
 
 }
--- src/main/java/ru/sberbank/data/Task.java
+++ src/main/java/ru/sberbank/data/Task.java
@@ -1,11 +1,57 @@
 package ru.sberbank.data;
 
 public class Task {
-    int id;
-    boolean isDeveloped;
+    private int id;
+    private boolean isDeveloped;
+    private boolean isTested;
+    private String summary;
 
-    public Task(int id) {
+    public Task(int id, String summary) {
         this.id = id;
         this.isDeveloped = false;
+        this.isTested = false;
+        this.summary = summary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public boolean isDeveloped() {
+        return isDeveloped;
+    }
+
+    public void setDeveloped(final boolean developed) {
+        isDeveloped = developed;
+    }
+
+    public boolean isTested() {
+        return isTested;
+    }
+
+    public void setTested(final boolean tested) {
+        isTested = tested;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(final String summary) {
+        this.summary = summary;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+               "id=" + id +
+               ", isDeveloped=" + isDeveloped +
+               ", isTested=" + isTested +
+               ", summary='" + summary + '\'' +
+               '}';
     }
 }
